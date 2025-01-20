08fd9f570aa333cece70b81fe71b931738e74337
tayutayu1229 <165854662+tayutayu1229@users.noreply.github.com>
2025-01-20 21:47:29 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,13 @@
 document.getElementById('login-form').addEventListener('submit', function(e) {
     e.preventDefault();
 
-    // ユーザー入力の取得
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-
-    // サンプルのユーザーデータ（実際にはサーバーから取得することが一般的）
     const users = [
         { username: 'trombone1727', password: 'hvPNh8qZ' },
         { username: 'Yoshihito', password: 'yoshi1229' },
         { username: 'Ayakawa', password: 'tXdQb8bfjBa6' },
     ];
-
-    // ログイン情報を検証
     const user = users.find(user => user.username === username && user.password === password);
 
     if (user) {
